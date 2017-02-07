c45f912b7b0a48483360e94f845b407927f611c3
Arnaldo Martins Hidalgo Junior <arnaldo@hidalgojunior.com.br>
2017-02-07 21:47:27 -0200

    Atualizaçao das informações

--- PIB/Resources/Resource.Designer.cs
+++ PIB/Resources/Resource.Designer.cs
@@ -41,6 +41,22 @@ namespace PIB
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int theme_devicedefault_background = 2131034112;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
@@ -53,6 +69,46 @@ namespace PIB
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f070004
+			public const int button1 = 2131165188;
+			
+			// aapt resource value: 0x7f070005
+			public const int button2 = 2131165189;
+			
+			// aapt resource value: 0x7f070006
+			public const int button3 = 2131165190;
+			
+			// aapt resource value: 0x7f070007
+			public const int button4 = 2131165191;
+			
+			// aapt resource value: 0x7f070008
+			public const int button5 = 2131165192;
+			
+			// aapt resource value: 0x7f070000
+			public const int linearLayout1 = 2131165184;
+			
+			// aapt resource value: 0x7f070001
+			public const int textView1 = 2131165185;
+			
+			// aapt resource value: 0x7f070002
+			public const int textView2 = 2131165186;
+			
+			// aapt resource value: 0x7f070003
+			public const int textView3 = 2131165187;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
 			{
 			}
 		}
@@ -88,6 +144,22 @@ namespace PIB
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f060000
+			public const int Theme_Custom = 2131099648;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
